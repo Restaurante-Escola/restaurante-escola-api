77e825d856c1b027d658bbe6378427e8f169af9c
Gabriel <gabriel.romano.3194@hotmail.com>
2021-12-08 14:43:51 -0300

    alteração nos testes unitários

--- src/test/java/br/com/restaurante/RestauranteApplicationTests.java
+++ src/test/java/br/com/restaurante/RestauranteApplicationTests.java
@@ -21,7 +21,7 @@ class RestauranteApplicationTests {
 	@Autowired
 	private AlunoService service;
 	
-	private Aluno aluno = new Aluno(666L, "Aluno Teste", "Teste", 1, "Teste", LocalDate.now(), "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", "Teste", false, false, false, false, "Teste", false, false, false, false, false, false, false, false, "Teste", "Teste");
+	private Aluno aluno = new Aluno(345749287L, "João Pedro", "João", 15, "Solteiro", LocalDate.now(), "213512353", "42352362313", "13982675622", "13982675622", "13982675622", "Casimiro", "Médio", "Segundo", "Liceu", "1 ano", "2030", "G", "45", "UNIMED", "gonzaga", "amanda", "0800329824", "3423423", "88273023", "4234234", false, false, false, false, "nenhum", false, false, false, false, false, false, false, false, "nenhum", "nenhum");
 	
 	@Test
 	public void crud() {
