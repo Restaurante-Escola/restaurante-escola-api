3978dbec63e3da68ee9a00d86184b788c6d4b990
Eduardo Neves <eduardo.neves@sch.srv.br>
2022-12-03 19:25:29 -0300

    Adição de mais testes

--- src/test/java/br/com/restaurante/RestauranteApplicationTests.java
+++ src/test/java/br/com/restaurante/RestauranteApplicationTests.java
@@ -1,10 +1,16 @@
 package br.com.restaurante;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.time.LocalDate;
 
+import org.assertj.core.error.ShouldBeGreater;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.internal.matchers.GreaterThan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,11 +35,18 @@ class RestauranteApplicationTests {
 	@Autowired
 	private AdvertenciaService AdvertenciaService;
 	
-	private Aluno aluno = new Aluno(345749287L, "João Pedro", "João", 15, "Solteiro", LocalDate.now(), "213512353", "42352362313", "13982675622", "13982675622", "13982675622", "Casimiro", "Médio", "Segundo", "Liceu", "1 ano", "2030", "G", "45", "UNIMED", "gonzaga", "amanda", "0800329824", "3423423", "88273023", "4234234", false, false, false, false, "nenhum", false, false, false, false, false, false, false, false, "nenhum", "nenhum");
-	private Turma turma = new Turma(444L, 444, LocalDate.now(),LocalDate.of(2023,11,16));
+	private Aluno aluno = new Aluno(345749287L, "João Pedro", "João", 15, "Solteiro", 
+			LocalDate.now(), "213512353", "42352362313", "13982675622", "13982675622", 
+			"13982675622", "Casimiro", "Médio", "Segundo", "Liceu", "1 ano", "2030", "G", 
+			"45", "UNIMED", "gonzaga", "amanda", "0800329824", "3423423", "88273023", 
+			"4234234", false, false, false, false, "nenhum", false, false, false, false, 
+			false, false, false, false, "nenhum", "nenhum");
+	
+	private Turma turma = new Turma(444, LocalDate.now(),LocalDate.of(2023,11,16));
+	
 	private Advertencia advertencia = new Advertencia(aluno,"bateu no amiguinho",LocalDate.now());
 	
-	@Test
+	
 	public void crud() {
 		create();
 		read();
@@ -70,28 +83,34 @@ class RestauranteApplicationTests {
 		Assert.assertTrue(delete);
 	}
 	
-	
+
 	public void createClass() {
+		int numeroTurma = turma.getNumero();
+		Turma turmaExistente = TurmaService.findByCodigoTurma(numeroTurma);
+		Assert.assertNull(turmaExistente);
+		assertTrue(turma.getFimTurma().isAfter(turma.getInicioTurma()));
 		Turma turmaCriada = TurmaService.create(turma);
 		Assert.assertNotNull(turmaCriada);
 	}
 	
 	
 	public void readClass() {
-		Turma turmaBuscada = TurmaService.findByCodigoTurma(444);
+		Turma turmaBuscada = TurmaService.findByCodigoTurma(turma.getNumero());
 		Assert.assertNotNull(turmaBuscada);
 	}
 	
 	
 	public void updateClass() {
-		turma.setFimTurma(LocalDate.of(2023,10,16));
+		turma.setFimTurma(LocalDate.of(2024,10,16));
+		assertTrue(turma.getFimTurma().isAfter(turma.getInicioTurma()));
 		boolean turmaUpdated = TurmaService.update(turma);
 		Assert.assertNotNull(turmaUpdated);
 	}
 	
 	
 	public void deleteClass() {
-		boolean turmaDeleted = TurmaService.delete(444);
+		readClass();
+		boolean turmaDeleted = TurmaService.delete(turma.getNumero());
 		Assert.assertTrue(turmaDeleted);
 	}
 	
